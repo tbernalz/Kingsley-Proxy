ef76ad111cf4adb59ea98db3b5b6e893351de8d8
Tomás Bernal <tbernalz@eafit.edu.co>
2025-05-09 21:49:29 -0500

    chore(docker): add docker stuff to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
